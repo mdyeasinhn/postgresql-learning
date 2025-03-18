93c4cdaa69806a170e564f83ec960e0c30edb5ac
mdyeasinhn <mdyeasinhn@gmail.com>
2025-03-18 16:32:40 +0600

    create a student table

--- alter.sql
+++ alter.sql
@@ -1,23 +1,26 @@
+-- get database table
 SELECT * from person2;
-
+-- set a column with default value 
 ALTER TABLE person2
 ADD COLUMN email VARCHAR(25) DEFAULT 'default@gmail.com' NOT NULL;
 
+-- insert value in column 
 INSERT into person2 values(4, 'test4', 22, 'test@gmail.com');
 
 
-
+-- delete a table 
 ALTER TABLE person2
 DROP email 
 
-
+-- rename a column 
 ALTER TABLE person2
 RENAME COLUMN age to user_age;
 
-
+-- set type type varchar in column 
 ALTER TABLE person2
 ALTER COLUMN  user_name TYPE VARCHAR(25);
 
+-- set not null
 ALTER TABLE person2
 ALTER COLUMN  user_age set NOT NULL;
 
---
+++ select.sql
@@ -0,0 +1,13 @@
+-- creat a student table 
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    age INT,
+    grade CHAR(2),
+    course VARCHAR(50),
+    email VARCHAR(100),
+    dob DATE,
+    blood_group VARCHAR(5),
+    country VARCHAR(50)
+)

ed5b7c1078ed0d6383fc5cd32f59bddffc4d7b83
mdyeasinhn <mdyeasinhn@gmail.com>
2025-03-19 17:01:37 +0600

    Enforcing Referential Integrity: Behaviors During Data Deletion

--- foreignKey.sql
+++ foreignKey.sql
@@ -8,12 +8,22 @@ CREATE TABLE "user" (
 CREATE TABLE post (  
     id SERIAL PRIMARY KEY,  
     title TEXT NOT NULL,  
-    user_id INTEGER REFERENCES "user"(id)  
+    user_id INTEGER REFERENCES "user"(id) ON DELETE CASCADE
+);  
+-- Create the "post" table with a foreign key reference to the "user" table
+CREATE TABLE post (  
+    id SERIAL PRIMARY KEY,  
+    title TEXT NOT NULL,  
+    user_id INTEGER REFERENCES "user"(id) ON DELETE set NULL
+);  
+CREATE TABLE post (  
+    id SERIAL PRIMARY KEY,  
+    title TEXT NOT NULL,  
+    user_id INTEGER REFERENCES "user"(id) ON DELETE set DEFAULT DEFAULT 2
 );  
 
 -- Alter 'post' table to ensure 'user_id' cannot be NULL
-ALTER TABLE post 
-ALTER COLUMN user_id SET NOT NULL;
+
 
 -- Insert users
 INSERT INTO "user" (username) VALUES  
@@ -27,12 +37,19 @@ INSERT INTO "user" (username) VALUES
 INSERT INTO post (title, user_id) VALUES  
 ('First Blog Post', 1),  
 ('Understanding PostgreSQL', 2),  
-('React vs Vue: A Comparison', 3),  
+('React vs Vue: A Comparison', 2),  
 ('Next.js Authentication Guide', 4),  
-('How to Optimize Web Performance', 5);
+('How to Optimize Web Performance', 4);
 
 -- Select all users
 SELECT * FROM "user";
 
 -- Select all posts
 SELECT * FROM post;
+
+
+DROP TABLE "user";
+DROP TABLE post
+
+DELETE FROM "user"
+    WHERE id =4

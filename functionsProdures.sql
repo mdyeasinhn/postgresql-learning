2484103844f14596bbe9fa27db1f3ef457a2eb79
mdyeasinhn <mdyeasinhn@gmail.com>
2025-03-24 03:53:59 +0600

    function delete an employee  with employee_id = 30

--- functionsProdures.sql
+++ functionsProdures.sql
@@ -12,3 +12,18 @@ $$;
 
 -- Calling the emp_count function to get the total number of employees
 SELECT emp_count();
+
+
+-- Function to delete an employee based on employee_id
+CREATE OR REPLACE FUNCTION delete_emp(emp_id INT)
+RETURNS VOID
+LANGUAGE SQL
+AS 
+$$
+DELETE FROM employees WHERE employee_id = emp_id;
+$$;
+
+
+-- Delete an employee with employee_id = 30
+SELECT delete_emp(30);
+    
